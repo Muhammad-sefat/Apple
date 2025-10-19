fdbafeaa249fc8650e58464617291334411e2a43
muhammad sefat <muhammadsefat55@gamil.com>
2025-10-19 19:08:13 +0600

    complete the full website

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
+import Features from "./components/Features";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
+import HowItWorks from "./components/HowItWorks";
+import Model from "./components/Model";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -8,6 +12,10 @@ function App() {
       <Navbar />
       <Hero />
       <Highlights />
+      <Model />
+      <Features />
+      <HowItWorks />
+      <Footer />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,13 @@ import { navLists } from "../constants";
 
 const Navbar = () => {
   return (
-    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
+    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center sticky top-0 bg-black z-50">
       <nav className="flex w-full screen-max-width">
-        <img src={appleImg} alt="Apple" width={14} height={18} />
+        <img
+          src={appleImg}
+          alt="Apple"
+          className="w-4 h-4 lg:w-6 lg:h-6 cursor-pointer"
+        />
 
         <div className="flex flex-1 justify-center max-sm:hidden">
           {navLists.map((nav) => (
